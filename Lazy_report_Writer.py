db8a2a53b85e6bf830d464d6f01677ad754aeed9
Shunto Dohi <112991989+202248SD@users.noreply.github.com>
2023-02-10 18:19:11 +0800

    Create Lazy_report_Writer.py

---
+++ Lazy_report_Writer.py
@@ -0,0 +1,15 @@
+import random
+
+a= ["Excellent.", "Amazing.", "Fantastic.",]
+b= ["they should work on", "they can improve on", ]
+c= ["Their work on Graphs was ", "Their overall grade was", "Most of their classwork was"]
+d=["If there is one thing"]
+e=["it would be organising data", "it would be using spreadsheets more effectively"]
+x = random.choice(a)
+y = random.choice(b)
+z = random.choice(c)
+w = random.choice(d)
+t = random.choice(e)
+print("Half term report")
+print(z, x, w, y, t)
+#GC29
