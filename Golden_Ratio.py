21ae5da8604290fe593bb63d1601968b9e1be49f
Shunto Dohi <112991989+202248SD@users.noreply.github.com>
2023-02-10 18:42:09 +0800

    Create Golden_Ratio.py

---
+++ Golden_Ratio.py
@@ -0,0 +1,5 @@
+length = int(input("what is the length, in cm of your painting:"))
+golden_ratio = 1.618
+height = round(length/golden_ratio)
+print("the height of your painting should be",height,"cm")
+#GC11
