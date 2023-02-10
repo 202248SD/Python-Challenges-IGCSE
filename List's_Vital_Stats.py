bd3b5aa9837281056c535faad83e61046f0c9086
Shunto Dohi <112991989+202248SD@users.noreply.github.com>
2023-02-10 18:15:16 +0800

    Create List's_Vital_Stats.py

---
+++ List's_Vital_Stats.py
@@ -0,0 +1,7 @@
+MyList = [1,2,3,4,5,6,7,8,9,10]
+print("Sum:",sum(MyList))
+print("Min:",min(MyList))
+print("Max:",max(MyList))
+print("Length:",len(MyList))
+print("Mean Average:",sum(MyList)/len(MyList))
+#TU13
