6db299cddadf7530d7f960de2a61d9ff80fdd02d
Shunto Dohi <112991989+202248SD@users.noreply.github.com>
2023-02-10 18:18:39 +0800

    Create The_Roasting_Machine.py

---
+++ The_Roasting_Machine.py
@@ -0,0 +1,6 @@
+import random 
+
+a = ["Of course I talk like an idiot, How else can you understand me?", "I treasure the time I don't spend with you", "You are so fat you fall of both sides of the bed", "You reminded me of a penny. Two faced and not worth much", "Life is full of dissapointments and I just added you to the list", "You sound like you eat ciggarates","Have you ever met a human version of a headache" ] 
+b = random.choice(a)
+print(b)
+#GC39
