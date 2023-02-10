0e01e34de65e2d37b34ace028e741827edca8b36
Shunto Dohi <112991989+202248SD@users.noreply.github.com>
2023-02-10 18:14:23 +0800

    Create Rugby_Calculator.py

---
+++ Rugby_Calculator.py
@@ -0,0 +1,13 @@
+a = input("What is the name of the first team?")
+b = input("What is the name of the Second team?")
+c = input("Number of tries the first team scored:")
+d = input("Number of tries the second team scored:")
+e = input("Number of penalties the first team scored:")
+f = input("Number of penalties the second team scored:")
+g = input("Number of conversions the first team scored:")
+h = input("Number of conversions the second team scored:")
+x = c * 5 + e * 3 + g * 7
+y = d * 5 + f * 3 + h * 7 
+print(a,"has",x,"points")
+print(b,"has",y,"points")
+#GC23
